c7d89568e41eaa4cc222af908b058d9784fb2a14
申耀波 <644100298@qq.com>
2023-08-03 16:16:48 +0800

    syb

--- src/main.js
+++ src/main.js
@@ -1,23 +1,26 @@
-import { createApp, reactive } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// 不再需要以下的导入语句
+// import { createApp, reactive } from 'vue'
+// import ElementPlus from 'element-plus'
+// import * as echarts from 'echarts';
+// import router from './router'
+// import store from './store'
+
 import 'normalize.css'
 import './assets/css/base.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import * as echarts from 'echarts';
 import LoadingButton from '@/components/LoadingButton.vue';
+import App from './App.vue'
 
-const app = createApp(App)
+// 通过 window 对象访问全局的 Vue、ElementPlus、echarts、Vuex 和 VueRouter 实例
+const { Vue, ElementPlus, echarts, Vuex, VueRouter } = window;
+const app = Vue.createApp(App)
 app.use(ElementPlus);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
 // 创建响应式对象作为全局变量，并设置一个默认的标签
-const globalState = reactive({
+const globalState = Vue.reactive({
     userInfo: {},
     openTabs: [],
 });
@@ -26,4 +29,4 @@ const globalState = reactive({
 app.provide('$global', globalState);
 app.provide('$echarts', echarts)
 app.component('loading-button', LoadingButton);
-app.use(store).use(router).mount('#app')
+app.use(Vuex).use(VueRouter).mount('#app')
