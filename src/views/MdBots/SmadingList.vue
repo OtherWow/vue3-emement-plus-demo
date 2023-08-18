9fe280cd463c9bf5fab406b667a2fc0c380d9f6a
申耀波 <644100298@qq.com>
2023-08-18 12:59:39 +0800

    Update SmadingList.vue

--- src/views/MdBots/SmadingList.vue
+++ src/views/MdBots/SmadingList.vue
@@ -6,32 +6,54 @@
                     <el-button type="primary" @click="connectToWebSocket()">重新连接websocket服务器</el-button>
                 </el-col>
             </el-row>
-            <el-table :data="smading_infos_list">
-                <el-table-column prop="name" label="交易所账号名称"></el-table-column>
-                <el-table-column prop="symbol" label="交易对"></el-table-column>
-                <el-table-column prop="最新价格" label="最新价格"></el-table-column>
-                <el-table-column prop="做空仓位数量" label="做空仓位数量"></el-table-column>
-                <el-table-column prop="做空仓位价格" label="做空仓位价格"></el-table-column>
-                <el-table-column prop="做空仓位浮动盈亏" label="做空仓位浮动盈亏"></el-table-column>
-                <el-table-column prop="做多仓位数量" label="做多仓位数量"></el-table-column>
-                <el-table-column prop="做多仓位价格" label="做多仓位价格"></el-table-column>
-                <el-table-column prop="做多仓位浮动盈亏" label="做多仓位浮动盈亏"></el-table-column>
-                <el-table-column prop="总浮动盈亏" label="总浮动盈亏"></el-table-column>
-                <el-table-column prop="做空总盈利" label="做空总盈利"></el-table-column>
-                <el-table-column prop="做多总盈利" label="做多总盈利"></el-table-column>
-                <el-table-column prop="总盈利" label="总盈利"></el-table-column>
-                <!-- <el-table-column prop="is_main" label="是否主账号" width="120">
+            <el-table :data="smading_infos_list" stripe style="width: 100%" :fit="false" border highlight-current-row
+                :summary-method="getSummaries" show-summary>
+                <el-table-column fixed="left" prop="name" label="交易所账号名称" width="130" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="left" prop="symbol" label="交易对" width="120" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="最新价格" label="最新价格" width="90" show-overflow-tooltip align="center"></el-table-column>
+                <el-table-column prop="第几次对冲单" label="第几次对冲单" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="第几次补单" label="第几次补单" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做空仓位数量" label="做空仓位数量" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做空仓位价格" label="做空仓位价格" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做空仓位浮动盈亏" label="做空仓位浮动盈亏" width="140" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做多仓位数量" label="做多仓位数量" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做多仓位价格" label="做多仓位价格" width="110" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column prop="做多仓位浮动盈亏" label="做多仓位浮动盈亏" width="140" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="right" prop="总浮动盈亏" label="总浮动盈亏" width="100" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="right" prop="做空总盈利" label="做空总盈利" width="100" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="right" prop="做多总盈利" label="做多总盈利" width="100" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="right" prop="总盈利" label="总盈利" width="80" show-overflow-tooltip
+                    align="center"></el-table-column>
+                <el-table-column fixed="right" prop="is_run" label="运行" width="60" show-overflow-tooltip align="center">
                     <template #default="{ row }">
-                        <el-tag :type="row.is_main ? 'success' : 'danger'">{{ row.is_main ? '是' : '否' }}</el-tag>
+                        <el-tag :type="row.is_run ? 'success' : 'danger'" effect="dark">{{ row.is_run ? '是' : '否'
+                        }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column label="操作" width="320">
+                <el-table-column fixed="right" label="操作" width="260" align="center">
                     <template #default="{ row }">
-                        <el-button type="primary" size="small" @click="setMainAccount(row)">设为主账号</el-button>
-                        <el-button type="info" size="small" @click="editExchange(row)">编辑</el-button>
-                        <el-button type="danger" size="small" @click="deleteExchange(row)">删除</el-button>
+                        <el-button type="primary" size="small" @click="startStrategy(row)" plain
+                            :disabled="row.is_run">启动</el-button>
+                        <el-button type="primary" size="small" @click="stopStrategy(row)" plain
+                            :disabled="!row.is_run">停止</el-button>
+                        <el-button type="primary" size="small" @click="editStrategy(row)" plain>编辑</el-button>
+                        <el-button type="danger" size="small" @click="deleteStrategy(row)"
+                            :disabled="row.is_run">删除</el-button>
                     </template>
-                </el-table-column> -->
+                </el-table-column>
             </el-table>
         </el-main>
     </el-container>
@@ -79,6 +101,37 @@ function connectToWebSocket() {
     ws.onerror = (error) => {
         console.log("WebSocket error:", error);
     };
+}
+
+const columnsToSummarize = ['做空仓位浮动盈亏', '做多仓位浮动盈亏', '总浮动盈亏', '做空总盈利', '做多总盈利', '总盈利'];
+const getSummaries = (param) => {
+    const { columns, data } = param
+    const sums = []
+    columns.forEach((column, index) => {
+        if (index === 0) {
+            sums[index] = '合计'
+            return
+        }
+        if (columnsToSummarize.includes(column.property)) {
+            const values = data.map((item) => Number(item[column.property]));
+            if (!values.every((value) => Number.isNaN(value))) {
+                sums[index] = `${values.reduce((prev, curr) => {
+                    const value = Number(curr);
+                    if (!Number.isNaN(value)) {
+                        return prev + curr;
+                    } else {
+                        return prev;
+                    }
+                }, 0).toFixed(2)}`;
+            } else {
+                sums[index] = '0';
+            }
+        } else {
+            sums[index] = ''; // 不进行统计的列设置为空字符串或其他适当的默认值
+        }
+    })
+
+    return sums
 }
 </script>
   
